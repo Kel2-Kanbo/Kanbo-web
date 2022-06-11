1a76088bf1bf22ddf6523d1a7d1e7690f299a3ad
Yovanta Anjelina <yovantaanjelina1001@gmail.com>
2022-06-11 14:44:37 +0700

    fix: button status

--- src/Components/TableRoom/index.jsx
+++ src/Components/TableRoom/index.jsx
@@ -15,52 +15,52 @@ export default function TableComplex(props) {
 
   return (
     <div>
-      <div class="flex flex-col">
+      <div className="flex flex-col">
         <div>
-          <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-            <div class="overflow-hidden">
-              <table class="min-w-full">
-                <thead class="bg-white">
+          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full">
+                <thead className="bg-white">
                   <tr>
                     <th
                       scope="col"
-                      class="text-sm font-medium text-textColor-black px-6 py-4 text-left"
+                      className="text-sm font-medium text-textColor-black px-6 py-4 text-left"
                     >
                       Picture
                     </th>
                     <th
                       scope="col"
-                      class="text-sm font-medium text-textColor-black px-6 py-4 text-left"
+                      className="text-sm font-medium text-textColor-black px-6 py-4 text-left"
                     >
                       Room Name
                     </th>
                     <th
                       scope="col"
-                      class="text-sm font-medium text-textColor-black px-6 py-4 text-left"
+                      className="text-sm font-medium text-textColor-black px-6 py-4 text-left"
                     >
                       Floor
                     </th>
                     <th
                       scope="col"
-                      class="text-sm font-medium text-textColor-black px-6 py-4 text-left"
+                      className="text-sm font-medium text-textColor-black px-6 py-4 text-left"
                     >
                       Room Item
                     </th>
                     <th
                       scope="col"
-                      class="text-sm font-medium text-textColor-black px-6 py-4 text-left"
+                      className="text-sm font-medium text-textColor-black px-6 py-4 text-left"
                     >
                       Rate per day
                     </th>
                     <th
                       scope="col"
-                      class="text-sm font-medium text-textColor-black px-6 py-4 text-left"
+                      className="text-sm font-medium text-textColor-black px-6 py-4 text-left"
                     >
                       Status
                     </th>
                     <th
                       scope="col"
-                      class="text-sm font-medium text-textColor-black px-6 py-4 text-left"
+                      className="text-sm font-medium text-textColor-black px-6 py-4 text-left"
                     >
                       Actions
                     </th>
@@ -68,31 +68,46 @@ export default function TableComplex(props) {
                 </thead>
                 <tbody>
                   {room?.map((room) => (
-                    <tr class="bg-secondary-blue">
-                      <td class="text-sm text-textColor-blackThin font-light px-6 py-4 whitespace-nowrap">
-                        <img src={room.picture} alt="room" width='32px'/>
+                    <tr className="bg-secondary-blue">
+                      <td className="text-sm text-textColor-blackThin font-light px-6 py-4 whitespace-nowrap">
+                        <img src={room.picture} alt="room" width="72px" />
                       </td>
-                      <td class="text-sm text-textColor-blackThin font-light px-6 py-4 whitespace-nowrap">
+                      <td className="text-sm text-textColor-blackThin font-light px-6 py-4 whitespace-nowrap">
                         {room.roomName}
                       </td>
-                      <td class="text-sm text-textColor-blackThin font-light px-6 py-4 whitespace-nowrap">
+                      <td className="text-sm text-textColor-blackThin font-light px-6 py-4 whitespace-nowrap">
                         {room.floor}
                       </td>
-                      <td class="text-sm text-textColor-blackThin font-light px-6 py-4 whitespace-nowrap">
-                        {room.roomItem}
+                      <td className="text-sm text-textColor-blackThin font-light px-6 py-4 whitespace-nowrap">
+                        {room.roomItem.map((item) => (
+                          <li className="list-none inline m-1">
+                            {item.itemName}
+                          </li>
+                        ))}
                       </td>
-                      <td class="text-sm text-textColor-blackThin font-light px-6 py-4 whitespace-nowrap">
+                      <td className="text-sm text-textColor-blackThin font-light px-6 py-4 whitespace-nowrap">
                         {room.roomPrice}
                       </td>
-                      <td class="text-sm text-textColor-blackThin font-light px-6 py-4 whitespace-nowrap">
-                        {room.status}
+                      <td className="text-sm text-textColor-blackThin font-light px-6 py-4 whitespace-nowrap">
+                        {room.status ? (
+                          <button className="bg-success-green text-textColor-green font-bold py-2 px-6 rounded-full">
+                            Available
+                          </button>
+                        ) : (
+                          <button className="bg-error-pink text-textColor-red font-bold py-2 px-6 rounded-full">
+                            Booked
+                          </button>
+                        )}
                       </td>
-                      <td class="flex justify-around px-6 py-4 whitespace-nowrap">
-                        <button onClick={() => removeRoom(room.id)}> 
+                      <td className="flex justify-around whitespace-nowrap">
+                        <button
+                          onClick={() => removeRoom(room.id)}
+                          className="px-2 py-4"
+                        >
                           <RiDeleteBin5Line />
                         </button>
-                        <button>
-                          <FiEdit/>
+                        <button className="px-2 py-4">
+                          <FiEdit />
                         </button>
                       </td>
                     </tr>
