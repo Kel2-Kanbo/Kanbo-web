bc3739a7878be56a6403e83f9d51b8492f9438b9
rvinara <ervinarosamada@gmail.com>
2022-06-15 01:13:14 +0700

    fix: create building and page Building

--- src/Pages/Building/index.jsx
+++ src/Pages/Building/index.jsx
@@ -2,15 +2,16 @@ import React, { useState, useEffect } from "react";
 import api from "../../API/Complex";
 
 import Button from "../../Components/Button";
-import CreateComplex from "../../Components/CreateComplex";
-import Navbar from "../../Components/Navbar";
 import Sidebar from "../../Components/Sidebar";
-import TableComplex from "../../Components/TableComplex";
+import Navbar from "../../Components/Navbar";
+import CreateBuilding from "../../Components/CreateBuilding";
+import TableBuilding from "../../Components/TableBuilding";
 
-const Building = () => {
+export default function Building() {
   const [showModal, setShowModal] = useState(false);
-  const [complex, setComplex] = useState([]);
-  console.log(complex);
+  console.log(showModal);
+  const [building, setBuilding] = useState([]);
+  console.log(building);
 
   const _handleOpenModal = () => {
     setShowModal(true);
@@ -19,91 +20,82 @@ const Building = () => {
     setShowModal(false);
   };
 
-  //get complex data from the server
-  const getComplex = async () => {
-    const response = await api.get("/complex");
+  const getBuilding = async () => {
+    const response = await api.get("/building");
     return response.data;
   };
 
-  //add complex
-  const addComplex = async (data) => {
-    const response = await api.post("/complex", data);
+  const addBuilding = async (data) => {
+    const response = await api.post("/building", data);
     if (response.data) {
-      setComplex([...complex, response.data]);
+      setBuilding([...building, response.data]);
       setShowModal(false);
     }
   };
 
-  //remove complex
-  const removeComplex = async (id) => {
-    const response = await api.delete(`/complex/${id}`);
+  const removeBuilding = async (id) => {
+    const response = await api.delete(`/building/${id}`);
     if (response.data) {
-      alert("Complex has been deleted");
-      setComplex(complex.filter((item) => item.id !== id));
+      alert("Delete building success");
+      setBuilding(building.filter((item) => item.id !== id));
     }
   };
 
-  //update complex
-  const updateComplex = async (data) => {
+  const updateBuilding = async (data) => {
     console.log(data);
     console.log(data.id);
-    const response = await api.put(`/complex/${data.id}`, data);
+    const response = await api.put(`/building/${data.id}`, data);
     const { id } = response.data;
     console.log(response.data);
-    setComplex(
-      complex.map((data) => {
+    setBuilding(
+      building.map((data) => {
         return data.id === id ? { ...response.data } : data;
       })
     );
     if (response.data) {
-      const allComplex = await getComplex();
-      setComplex(allComplex);
-      setShowModal(false);
+      const allBuilding = await getBuilding();
+      setBuilding(allBuilding);
     }
   };
 
   useEffect(() => {
-    const getAllComplex = async () => {
-      const allComplex = await getComplex();
-      if (allComplex) {
-        setComplex(allComplex);
+    const getAllBuilding = async () => {
+      const allBuilding = await getBuilding();
+      if (allBuilding) {
+        setBuilding(allBuilding);
       }
     };
-    getAllComplex();
+    getAllBuilding();
   }, []);
 
-  return (
-    <div className='flex h-screen bg-secondary-softblue'>
-        <Sidebar />
-        <Navbar />
-      <div className='basis-5/6'>
-        <div className="px-4 py-4 mt-20">
-          <h1 className="text-3xl font-bold mb-1">Building</h1>
-          <h4 className="text-md text-primary-gray">Manage Building</h4>
-          <div className="flex justify-end">
-            <div className="w-auto">
-              <Button type="button" className="bg-primary-blue text-primary-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                onClick={_handleOpenModal}>
-                Create Complex
-              </Button>
-            </div>
-            {showModal ? (
-              <CreateComplex
-                handleClose={_handleCloseModal}
-                addComplex={addComplex}
-              />
-            ) : null}
-          </div>
-          <div className="bg-primary-white items-center rounded mt-4">
-            <TableComplex
-              complex={complex}
-              removeComplex={removeComplex}
-              updateComplex={updateComplex}
-            />
-          </div>
+  return ( 
+    <div className="bg-secondary-blue h-screen">
+      <Sidebar/>
+      <Navbar/>
+      <h1 className="text-3xl p-4">Building</h1>
+      <div className="text-sm">
+        <select className="bg-secondary-blue text-textColor-blackThin font-light px-6 py-4 whitespace-nowrap">
+          <option value="" className="bg-secondary-blue text-textColor-blackThin font-light px-6 py-4 whitespace-nowrap">Sort by</option>
+        </select>
+       </div>
+
+      <div className="flex justify-end">
+        <div className="w-auto p-8">
+          <Button type="button" style={{backgroundColor: "blue", color: "white"}} onClick={ _handleOpenModal}>
+            Create Building
+          </Button>
         </div>
+        {showModal ?(
+          <CreateBuilding handleClose={_handleCloseModal} addBuilding={addBuilding} />
+          ) : (null)}
       </div>
-    </div >
-  )
+      <div className="bg-primary-white items-center m-4">
+        <TableBuilding
+          building={building}
+          removeBuilding={removeBuilding}
+          updateBuilding={updateBuilding}
+        />
+      </div>
+    </div>
+  );
 }
-export default Building;
