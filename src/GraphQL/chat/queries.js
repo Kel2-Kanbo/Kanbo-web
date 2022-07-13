84d2d1673c65e44bc8a41ffb9c436708e38e2205
Shironim <dimasseto.id@gmail.com>
2022-07-13 08:16:48 +0700

    bug cant filtering chat room

--- src/GraphQL/chat/queries.js
+++ src/GraphQL/chat/queries.js
@@ -10,4 +10,24 @@ subscription MySubscription($from: String) {
     to
   }
 }
-`
+`;
+
+export const INSERT_ROOM_CHAT = gql`
+mutation MyMutation($user1: String, $user2: String) {
+  insert_kanbo_chat_roomChat(objects: {user1: $user1, user2: $user2}) {
+    returning {
+      id_room
+    }
+  }
+}
+`;
+
+export const GET_ROOM_CHAT = gql`
+query MyQuery {
+  kanbo_chat_roomChat {
+    id_room
+    user1
+    user2
+  }
+}
+`;
--- src/Pages/Chat/index.jsx
+++ src/Pages/Chat/index.jsx
@@ -3,10 +3,115 @@ import NavBar from '../../Components/Navbar';
 import Sidebar from '../../Components/Sidebar';
 import pic1 from "../../Assets/User3.png"
 import pic2 from "../../Assets/User2.png"
-import { GET_CHATS } from '../../GraphQL/chat/queries';
+import { GET_CHATS, INSERT_ROOM_CHAT, GET_ROOM_CHAT } from '../../GraphQL/chat/queries';
 import { FaSignOutAlt, FaChartLine } from "react-icons/fa";
 import { useState } from 'react';
+// Apollo Client
+import { useLazyQuery, useMutation, useSubscription } from '@apollo/client';
+import { useEffect } from 'react';
+
+import Swal from "sweetalert2";
+
 const Chat = () => {
+  const { data, loading, error } = useSubscription(GET_CHATS, { variables: { from: "Yovanta" } });
+  // console.log(data);
+  const [friend, setFriend] = useState([
+    {
+      user: "Yovanta",
+      message: "hii",
+    },
+    {
+      user: "Vina",
+      message: "haloo",
+    }
+  ]);
+  const [room, setRoom] = useState({
+    roomId: "",
+    user: "Dimas",
+    friend: "",
+  });
+  const [insertRoom, { loading: loadingInsertRoom }] = useMutation(INSERT_ROOM_CHAT, {
+    onCompleted: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      console.log('Terjadi error di mutasi insert', { error });
+    }
+  });
+
+  const cekRoomExist = (data) => {
+    const cek = data.filter((item) => {
+      return item.user1 === room.user && item.user2 === room.friend;
+    });
+
+    if (cek.length > 0) {
+      console.log("ini cek", cek);
+      setRoom({ ...room, roomId: cek[0].id_room });
+
+      // cek.map((item) => {
+      // });
+    } else {
+      console.log("tidak ada");
+    }
+    console.log('setelah cek ada isinya', room);
+  }
+
+
+  const [getRoomChat, { data: dataRoom, loading: loadingIdUpdate, refetch }] = useLazyQuery(GET_ROOM_CHAT, {
+    onCompleted: (dataRoom) => {
+      cekRoomExist(dataRoom.kanbo_chat_roomChat);
+    },
+    onError: (error) => {
+      console.log('Terjadi error di getDataUpdate lazyQuery', { error });
+    }
+  });
+  console.log('ini data room', dataRoom)
+  const tambahRoom = () => {
+    // console.log(room);
+    insertRoom({
+      variables: {
+        user1: room.user,
+        user2: room.friend,
+      }
+    });
+  }
+  const getData = () => {
+    if (!dataRoom) {
+      getRoomChat();
+      console.log("ini data room")
+    } else {
+      refetch();
+      console.log("refetch");
+    }
+  };
+  // console.log(data.kanbo_chat_chat);
+  // if (error) {
+  //   console.log(error)
+  // }
+  const getClick = (user) => {
+    setRoom({ ...room, friend: user });
+
+    Swal.fire({
+      title: "Yakin logout?",
+      text: "Jika ingin masuk silahkan login kembali!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, logout!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        getData();
+        Swal.fire("Berhasil!", "success");
+      }
+    });
+  }
+  useEffect(() => {
+    return console.log('ini room', room)
+  }, []);
+  // console.log(cek)
+  // console.log(room);
+
   return (
     <div className='flex h-screen'>
       <Sidebar />
@@ -22,124 +127,114 @@ const Chat = () => {
                       Search
                     </div>
                     <div className='flex flex-col'>
-                      <div className='chat flex p-4'>
-                        <img src={pic2} alt="room" className='rounded-full mr-2 h-14' />
-                        <div className='container flex-col self-center'>
-                          <div className='flex justify-between'>
-                            <span><b>Samantha Rachel</b></span>
-                            <span className='text-primary-gray'>17 Jun</span>
-                          </div>
-                          <div className='flex justify-between'>
-                            <span className='text-sm text-primary-gray'>Thankyou for your attention</span>
-                            {/* <span className='px-1 text-primary-blue bg-secondary-softblue rounded-full'>2</span> */}
-                          </div>
-                        </div>
-                      </div>
-                      <div className='chat flex p-4'>
-                        <img src={pic1} alt="room" className='rounded-full mr-2 h-14' />
-                        <div className='container flex-col self-center'>
-                          <div className='flex justify-between'>
-                            <span><b>Samantha Rachel</b></span>
-                            <span className='text-primary-gray'>17 Jun</span>
-                          </div>
-                          <div className='flex justify-between'>
-                            <span className='text-sm text-primary-gray'>Thankyou for your attention</span>
-                            {/* <span className='px-1 text-primary-blue bg-secondary-softblue rounded-full'>2</span> */}
-                          </div>
-                        </div>
-                      </div>
-                      <div className='chat flex p-4'>
-                        <img src={pic1} alt="room" className='rounded-full mr-2 h-14' />
-                        <div className='container flex-col self-center'>
-                          <div className='flex justify-between'>
-                            <span><b>Samantha Rachel</b></span>
-                            <span className='text-primary-gray'>17 Jun</span>
-                          </div>
-                          <div className='flex justify-between'>
-                            <span className='text-sm text-primary-gray'>Thankyou for your attention</span>
-                            <span className='px-1 text-primary-blue bg-secondary-softblue rounded-full'>2</span>
-                          </div>
-                        </div>
-                      </div>
+                      {
+                        friend.map((item, index) => {
+                          return (
+                            <div onClick={() => { getClick(item.user) }} className='chat flex p-4'>
+                              <img src={pic2} alt="room" className='rounded-full mr-2 h-14' />
+                              <div className='container flex-col self-center'>
+                                <div className='flex justify-between'>
+                                  <span><b>{item.user}</b></span>
+                                  <span className='text-primary-gray'>17 Jun</span>
+                                </div>
+                                <div className='flex justify-between'>
+                                  <span className='text-sm text-primary-gray'>{item.message}</span>
+                                  {/* <span className='px-1 text-primary-blue bg-secondary-softblue rounded-full'>2</span> */}
+                                </div>
+                              </div>
+                            </div>
+                          )
+                        }
+                        )
+                      }
 
                     </div>
                   </div>
                 </div>
                 <div className='basis-2/3 relative'>
-                  <div className='border-b-2'>
-                    <div className='m-6 flex'>
-                      <img src={pic1} alt="room" className='rounded-full mr-2 h-14' />
-                      <div className='container flex-col self-center'>
-                        <span><b>Samantha Rachel</b></span>
-                        <span className='text-sm text-primary-gray'>@samawa</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div style={{ height: "540px" }} className='container flex-col overflow-auto pb-8'>
-                    {/* <div className='my-8 container justify-center'>
+                  {
+                    loadingIdUpdate || loadingInsertRoom ? <h1>Loading</h1> :
+                      room.roomId !== "" ? (
+                        <>
+                          {
+                            <span>{room.roomId}</span>
+                          }
+                          <div className='border-b-2'>
+                            <div className='m-6 flex'>
+                              <img src={pic1} alt="room" className='rounded-full mr-2 h-14' />
+                              <div className='container flex-col self-center'>
+                                <span><b>Samantha Rachel</b></span>
+                                <span className='text-sm text-primary-gray'>@samawa</span>
+                              </div>
+                            </div>
+                          </div>
+                          <div style={{ height: "540px" }} className='container flex-col overflow-auto pb-8'>
+                            {/* <div className='my-8 container justify-center'>
                       <span className='text-center py-1 px-3 border-2 rounded-full'>
                         Fri, 18 Jun
                       </span>
                     </div> */}
-                    <div className='container p-4 flex-col'>
-                      <div className='flex'>
-                        <div className='max-w-xl p-4 bg-secondary-softblue rounded'>
-                          <span>
-                            <b>Samantha Rachel</b>
-                          </span>
-                          <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                          </p>
-                        </div>
-                      </div>
-                      <div className='flex mt-8 justify-end'>
-                        <div className='max-w-xl p-4 bg-secondary-softblue rounded'>
-                          <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                          </p>
-                        </div>
-                      </div>
-                      <div className='flex mt-8 justify-end'>
-                        <div className='max-w-xl p-4 bg-secondary-softblue rounded'>
-                          <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                          </p>
-                        </div>
-                      </div>
-                      <div className='flex mt-8 justify-end'>
-                        <div className='max-w-xl p-4 bg-secondary-softblue rounded'>
-                          <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                          </p>
-                        </div>
-                      </div>
-                      <div className='flex mt-8 justify-end'>
-                        <div className='max-w-xl p-4 bg-secondary-softblue rounded'>
-                          <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                          </p>
-                        </div>
-                      </div>
-                      <div className='flex mt-8'>
-                        <div className='max-w-xl p-4 bg-secondary-softblue rounded'>
-                          <span>
-                            <b>Samantha Rachel</b>
-                          </span>
-                          <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <form className="container absolute bottom-0 left-0 right-0 bg-primary-blue py-3 px-4">
-                    <input className=' bg-secondary-softblue w-full py-1 px-4 rounded' type="text" placeholder='123' />
-                    <div className='ml-4' >
-                      <button className='py-1 px-4 rounded ' style={{ background: "white" }}>
-                        Send
-                      </button>
-                    </div>
-                  </form>
+                            <div className='container p-4 flex-col'>
+                              <div className='flex'>
+                                <div className='max-w-xl p-4 bg-secondary-softblue rounded'>
+                                  <span>
+                                    <b>Samantha Rachel</b>
+                                  </span>
+                                  <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                  </p>
+                                </div>
+                              </div>
+                              <div className='flex mt-8 justify-end'>
+                                <div className='max-w-xl p-4 bg-secondary-softblue rounded'>
+                                  <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                  </p>
+                                </div>
+                              </div>
+                              <div className='flex mt-8 justify-end'>
+                                <div className='max-w-xl p-4 bg-secondary-softblue rounded'>
+                                  <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                  </p>
+                                </div>
+                              </div>
+                              <div className='flex mt-8 justify-end'>
+                                <div className='max-w-xl p-4 bg-secondary-softblue rounded'>
+                                  <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                  </p>
+                                </div>
+                              </div>
+                              <div className='flex mt-8 justify-end'>
+                                <div className='max-w-xl p-4 bg-secondary-softblue rounded'>
+                                  <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                  </p>
+                                </div>
+                              </div>
+                              <div className='flex mt-8'>
+                                <div className='max-w-xl p-4 bg-secondary-softblue rounded'>
+                                  <span>
+                                    <b>Samantha Rachel</b>
+                                  </span>
+                                  <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <form className="container absolute bottom-0 left-0 right-0 bg-primary-blue py-3 px-4">
+                            <input className=' bg-secondary-softblue w-full py-1 px-4 rounded' type="text" placeholder='123' />
+                            <div className='ml-4' >
+                              <button className='py-1 px-4 rounded ' style={{ background: "white" }}>
+                                Send
+                              </button>
+                            </div>
+                          </form>
+                        </>) : (<span>isi kosong</span>)
+                  }
                 </div>
               </div>
             </div>
